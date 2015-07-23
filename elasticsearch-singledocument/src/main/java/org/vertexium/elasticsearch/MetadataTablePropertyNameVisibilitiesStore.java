6de87cc5829ba9179d016c8944010f33838562f0
Joe Ferner <joe@fernsroth.com>
2015-07-23 13:47:07 -0400

    do some minimal level of caching in the MetadataTablePropertyNameVisibilitiesStore

--- elasticsearch-singledocument/src/main/java/org/vertexium/elasticsearch/MetadataTablePropertyNameVisibilitiesStore.java
+++ elasticsearch-singledocument/src/main/java/org/vertexium/elasticsearch/MetadataTablePropertyNameVisibilitiesStore.java
@@ -1,32 +1,67 @@
 package org.vertexium.elasticsearch;
 
 import com.google.common.hash.Hashing;
+import org.cache2k.Cache;
+import org.cache2k.CacheBuilder;
+import org.cache2k.CacheSource;
 import org.vertexium.Authorizations;
 import org.vertexium.Graph;
 import org.vertexium.GraphMetadataEntry;
 import org.vertexium.Visibility;
+import org.vertexium.util.VertexiumLogger;
+import org.vertexium.util.VertexiumLoggerFactory;
 
+import java.io.Serializable;
 import java.nio.charset.Charset;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MetadataTablePropertyNameVisibilitiesStore extends PropertyNameVisibilitiesStore {
+    private static final VertexiumLogger LOGGER = VertexiumLoggerFactory.getLogger(MetadataTablePropertyNameVisibilitiesStore.class);
     public static final String METADATA_PREFIX = "propertyNameVisibility.";
     private static final Charset UTF8 = Charset.forName("utf8");
+    private final Cache<String, Hashes> hashesCache;
     private Map<String, Visibility> visibilityCache = new HashMap<>();
 
+    public MetadataTablePropertyNameVisibilitiesStore() {
+        this.hashesCache = CacheBuilder
+                .newCache(String.class, Hashes.class)
+                .name(MetadataTablePropertyNameVisibilitiesStore.class, "hashesCache-" + System.identityHashCode(this))
+                .maxSize(10000)
+                .expiryDuration(60, TimeUnit.SECONDS)
+                .source(new CacheSource<String, Hashes>() {
+                    @Override
+                    public Hashes get(String o) throws Throwable {
+                        return null;
+                    }
+                })
+                .build();
+    }
+
     public Collection<String> getHashes(Graph graph, String propertyName, Authorizations authorizations) {
-        List<String> hashes = new ArrayList<>();
-        String prefix = getMetadataPrefixWithPropertyName(propertyName);
-        for (GraphMetadataEntry metadata : graph.getMetadataWithPrefix(prefix)) {
-            String visibilityString = metadata.getKey().substring(prefix.length());
-            Visibility visibility = getVisibility(visibilityString);
-            if (authorizations.canRead(visibility)) {
-                String hash = (String) metadata.getValue();
-                hashes.add(hash);
-            }
-        }
-        if (hashes.size() == 0) {
+        Hashes hashes = getHashes(graph, propertyName);
+        Collection<String> results = hashes.get(authorizations);
+        if (results.size() == 0) {
             throw new VertexiumNoMatchingPropertiesException(propertyName);
+        }
+        return results;
+    }
+
+    private Hashes getHashes(Graph graph, String propertyName) {
+        Hashes hashes = this.hashesCache.get(propertyName);
+        if (hashes == null) {
+            LOGGER.debug("cache miss for property: %s", propertyName);
+            hashes = new Hashes();
+            String prefix = getMetadataPrefixWithPropertyName(propertyName);
+            for (GraphMetadataEntry metadata : graph.getMetadataWithPrefix(prefix)) {
+                String visibilityString = metadata.getKey().substring(prefix.length());
+                Visibility visibility = getVisibility(visibilityString);
+                hashes.add(visibility, (String) metadata.getValue());
+            }
+            this.hashesCache.put(propertyName, hashes);
         }
         return hashes;
     }
@@ -41,15 +76,16 @@ public class MetadataTablePropertyNameVisibilitiesStore extends PropertyNameVisi
     }
 
     public String getHash(Graph graph, String propertyName, Visibility visibility) {
-        String visibilityString = visibility.getVisibilityString();
-        String metadataKey = getMetadataKey(propertyName, visibilityString);
-        String hash = (String) graph.getMetadata(metadataKey);
+        Hashes hashes = getHashes(graph, propertyName);
+        String hash = hashes.get(visibility);
         if (hash != null) {
             return hash;
         }
-
+        String visibilityString = visibility.getVisibilityString();
+        String metadataKey = getMetadataKey(propertyName, visibilityString);
         hash = Hashing.murmur3_128().hashString(visibilityString, UTF8).toString();
         graph.setMetadata(metadataKey, hash);
+        hashes.add(visibility, hash);
         return hash;
     }
 
@@ -59,5 +95,27 @@ public class MetadataTablePropertyNameVisibilitiesStore extends PropertyNameVisi
 
     private String getMetadataKey(String propertyName, String visibilityString) {
         return getMetadataPrefixWithPropertyName(propertyName) + visibilityString;
+    }
+
+    private static class Hashes implements Serializable {
+        private Map<Visibility, String> hashes = new HashMap<>();
+
+        public void add(Visibility visibility, String hash) {
+            hashes.put(visibility, hash);
+        }
+
+        public Collection<String> get(Authorizations authorizations) {
+            Collection<String> results = new ArrayList<>();
+            for (Map.Entry<Visibility, String> vh : hashes.entrySet()) {
+                if (authorizations.canRead(vh.getKey())) {
+                    results.add(vh.getValue());
+                }
+            }
+            return results;
+        }
+
+        public String get(Visibility visibility) {
+            return hashes.get(visibility);
+        }
     }
 }
