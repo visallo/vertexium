beb204287c95cc479ebb3e6381d3bd660ed1ebff
Matt Wizeman <matt.wizeman@gmail.com>
2016-06-09 10:12:36 -0400

    add a simple test for the new isEmpty method

---
+++ core/src/test/java/org/vertexium/util/IterableUtilsTest.java
@@ -0,0 +1,22 @@
+package org.vertexium.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4.class)
+public class IterableUtilsTest {
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(IterableUtils.isEmpty(Collections.emptyList()));
+        assertFalse(IterableUtils.isEmpty(Collections.singletonList("junit")));
+    }
+
+}
