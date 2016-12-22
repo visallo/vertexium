3ae4548cd69fa1c8d3cd372f051fe9c7358abe63
Joe Ferner <joe@fernsroth.com>
2016-12-21 22:27:04 -0500

    Added: Edge.getVertices which accepts fetch hints

--- inmemory/src/main/java/org/vertexium/inmemory/InMemoryEdge.java
+++ inmemory/src/main/java/org/vertexium/inmemory/InMemoryEdge.java
@@ -72,6 +72,11 @@ public class InMemoryEdge extends InMemoryElement<InMemoryEdge> implements Edge 
 
     @Override
     public EdgeVertices getVertices(Authorizations authorizations) {
+        return getVertices(FetchHint.ALL, authorizations);
+    }
+
+    @Override
+    public EdgeVertices getVertices(EnumSet<FetchHint> fetchHints, Authorizations authorizations) {
         return new EdgeVertices(
                 getVertex(Direction.OUT, authorizations),
                 getVertex(Direction.IN, authorizations)
