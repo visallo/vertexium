e8440cc9d91b8ad183bbd38ed22fb3e8403c7693
Joe Ferner <joe@fernsroth.com>
2017-05-17 18:23:19 -0400

    Fixed: SQL SPV loading when timestamp is out of sync with property

--- sql/src/main/java/org/vertexium/sql/SqlStreamingPropertyValueRef.java
+++ sql/src/main/java/org/vertexium/sql/SqlStreamingPropertyValueRef.java
@@ -3,16 +3,25 @@ package org.vertexium.sql;
 import org.vertexium.Visibility;
 import org.vertexium.property.StreamingPropertyValue;
 import org.vertexium.property.StreamingPropertyValueRef;
+import org.vertexium.util.VertexiumLogger;
+import org.vertexium.util.VertexiumLoggerFactory;
 
 class SqlStreamingPropertyValueRef extends StreamingPropertyValueRef<SqlGraph> {
+    private static final VertexiumLogger LOGGER = VertexiumLoggerFactory.getLogger(SqlStreamingPropertyValueRef.class);
     private final String elementId;
     private final String key;
     private final String name;
     private final Visibility visibility;
     private final long timestamp;
 
-    public SqlStreamingPropertyValueRef(StreamingPropertyValue propertyValue, String elementId, String key, String name,
-                                        Visibility visibility, long timestamp) {
+    public SqlStreamingPropertyValueRef(
+            StreamingPropertyValue propertyValue,
+            String elementId,
+            String key,
+            String name,
+            Visibility visibility,
+            long timestamp
+    ) {
         super(propertyValue);
         this.elementId = elementId;
         this.key = key;
@@ -23,7 +32,15 @@ class SqlStreamingPropertyValueRef extends StreamingPropertyValueRef<SqlGraph> {
 
     @Override
     public StreamingPropertyValue toStreamingPropertyValue(SqlGraph graph, long timestamp) {
-        return graph.getStreamingPropertyTable().get(elementId, key, name, visibility, timestamp)
-                .store(isStore()).searchIndex(isSearchIndex());
+        StreamingPropertyValue spv = graph.getStreamingPropertyTable()
+                .get(elementId, key, name, visibility, timestamp);
+        if (spv == null) {
+            LOGGER.warn("Could not find SQL SPV with the property timestamp %d, using ref timestamp instead", timestamp, this.timestamp);
+            spv = graph.getStreamingPropertyTable()
+                    .get(elementId, key, name, visibility, this.timestamp);
+        }
+        return spv
+                .store(isStore())
+                .searchIndex(isSearchIndex());
     }
 }
