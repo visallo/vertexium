6565a6ee1089cae6e58cbd20b092548af640051a
Joe Ferner <joe@fernsroth.com>
2018-01-21 20:22:25 -0500

    add cypher date functions year, month, day

---
+++ cypher/src/main/java/org/vertexium/cypher/functions/date/YearFunction.java
@@ -0,0 +1,13 @@
+package org.vertexium.cypher.functions.date;
+
+import org.vertexium.cypher.VertexiumCypherQueryContext;
+import org.vertexium.cypher.executor.ExpressionScope;
+
+import java.time.ZonedDateTime;
+
+public class YearFunction extends CypherUnaryDateFunction {
+    @Override
+    protected Object invokeZonedDateTime(VertexiumCypherQueryContext ctx, ZonedDateTime date, ExpressionScope scope) {
+        return date.getYear();
+    }
+}
