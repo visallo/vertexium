f5f1d4dc10d799e92f7e4c95cf04a54483d07bb2
Joe Ferner <joe@fernsroth.com>
2018-01-22 16:26:26 -0500

    add getId to DeleteExtendedDataRowEvent

--- core/src/main/java/org/vertexium/event/DeleteExtendedDataRowEvent.java
+++ core/src/main/java/org/vertexium/event/DeleteExtendedDataRowEvent.java
@@ -11,6 +11,10 @@ public class DeleteExtendedDataRowEvent extends GraphEvent {
         this.id = id;
     }
 
+    public ExtendedDataRowId getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return "DeleteExtendedDataRowEvent{" +
