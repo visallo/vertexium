5cfa54b928eae3a24188398cee28e9044d3e8db6
Joe Ferner <joe@fernsroth.com>
2020-01-07 14:50:57 -0500

    Remove log4j 1.x

--- elasticsearch5/search-index/src/test/java/org/vertexium/elasticsearch5/utils/ConsoleAppender.java
+++
@@ -1,7 +0,0 @@
-package org.vertexium.elasticsearch5.utils;
-
-public class ConsoleAppender extends org.apache.log4j.ConsoleAppender {
-    @Override
-    public synchronized void close() {
-    }
-}
