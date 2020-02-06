4d8db7c9aaf33297dfded082358f5d3677217948
Matt Wizeman <matt.wizeman@gmail.com>
2020-02-06 14:50:32 -0500

    added the name of the property when handling invalid shapes

--- elasticsearch5/search-index/src/main/java/org/vertexium/elasticsearch5/Elasticsearch5SearchIndex.java
+++ elasticsearch5/search-index/src/main/java/org/vertexium/elasticsearch5/Elasticsearch5SearchIndex.java
@@ -2383,7 +2383,7 @@ public class Elasticsearch5SearchIndex implements SearchIndex, SearchIndexWithVe
         try {
             geoShape.validate();
         } catch (VertexiumInvalidShapeException ve) {
-            LOGGER.warn("Attempting to repair invalid GeoShape", ve);
+            LOGGER.warn("Attempting to repair invalid GeoShape for property " + propertyNameWithVisibility, ve);
             geoShape = GeoUtils.repair(geoShape);
         }
 
